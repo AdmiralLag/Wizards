c745eb456b2f4e7b0281f7670b6c7d826bcb4a6a
AdmiralLag <123410194+AdmiralLag@users.noreply.github.com>
2023-05-06 17:26:37 -0500

    render Step

---
+++ db/init.js
@@ -0,0 +1,17 @@
+const { Client } = require('pg');
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+async function seed() {
+  await client.connect();
+
+  // Write your code to seed the database here
+
+  await client.end();
+}
+
+seed();
